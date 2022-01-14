e920b7f7cf2f872f8119a104e4e8c344ab826a64
6tankumar <cpattar3@gmail.com>
2022-01-14 18:21:57 +0530

    first commit

--- B17BEcom/src/app/app.module.ts
+++ B17BEcom/src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgMaterialModule } from './ng-material/ng-material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { SnackbarService } from './services/snackbar.service';
+import { UserRegistrationComponent } from './components/user-registration/user-registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserRegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgMaterialModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [SnackbarService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
